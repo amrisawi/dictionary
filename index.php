d907a2e979b61ba6ec70ac7dbbeb49852d5a16b1
root <root@amr-lient.myguest.virtualbox.org>
2025-01-15 03:06:26 +0200

    test

---
+++ index.php
@@ -0,0 +1,23 @@
+
+<form method="post">
+    <input type="text" name="word" placeholder="Enter a word">
+    <button type="submit">Search</button>
+</form>
+<?php
+$conn = new mysqli('localhost', 'root', 'Pass_2002', 'DictionaryDB');
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $word = $_POST['word'];
+    $sql = "SELECT definition FROM words WHERE word='$word'";
+    $result = $conn->query($sql);
+    if ($result->num_rows > 0) {
+        $word = $_POST['word'];
+        $row = $result->fetch_assoc();
+        echo "Word: ". $_POST['word'] . "<br>Definition: " . $row['definition'];
+    } else {
+        echo "Word not found.";
+    }
+}
+?>
